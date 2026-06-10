f3b8248c08f9ae39a843520da5009f69995a6fb0
Ansh2906-dev <anshchaudhari291206@gmail.com>
2026-06-11 02:18:21 +0530

    added another program

--- structures/structures1.c
+++ structures/structures1.c
@@ -1,2 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 
+struct student {
+
+
+    char name[100];
+    int roll_no;
+    float cgpa;
+    
+
+};
+
+int main(){
+  struct student s1 = {"ansh", 04, 7.7};
+  printf("\nroll no of student is : %d", s1.roll_no);
+
+
+
+   return 0; 
+}
