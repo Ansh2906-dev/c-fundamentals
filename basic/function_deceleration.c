f31dea475b97d6364d89dd4aa68610c8b8e75334
Ansh2906-dev <anshchaudhari291206@gmail.com>
2026-06-19 21:33:15 +0530

    added another program

---
+++ basic/function_deceleration.c
@@ -0,0 +1,20 @@
+//program wih function call
+
+#include<stdio.h>
+// functon deceleration
+
+void printhello();
+//main functon
+
+int main() {
+    //function call
+    printhello();
+
+    return 0;
+}
+
+//function defination
+void printhello() {
+
+    printf("hello world\n");
+}
